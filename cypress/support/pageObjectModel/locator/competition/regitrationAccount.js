ab9aa788ffa1cc321dd378fbe867219c31c3c774
hoavu0109 <vuhoaptit@gmail.com>
2024-03-15 18:03:14 +0700

    update locator

---
+++ cypress/support/pageObjectModel/locator/competition/regitrationAccount.js
@@ -0,0 +1,7 @@
+class RegistrationLocators {
+    enterChineseCorporateName ='#basic_participatingUnitChineseName'
+    enterParticipatingUnitAbbreviation ='#basic_participatingUnitEnglishName'
+    enterLegalEntity ='#basic_legalEntity'
+    enterCorporateName ='#basic_corporateNameAbbr'
+    enterChineseName ='#basic_chineseName'
+}
